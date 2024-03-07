4be9d7ec29f84ac43f653bd5db2226b25f29b419
jose-tl <j.tonnesenlucas@gmail.com>
2024-03-07 10:07:07 +0100

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "digi_kultur_scraping"
+include(":app")
